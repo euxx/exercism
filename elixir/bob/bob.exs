f355e0642352683072e3a4f3ffbbbe8d7d9dee35
eux <euxuuu@gmail.com>
2019-02-24 23:51:01 +0800

    Improve Elixir Track - bob

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -3,24 +3,19 @@ import String
 defmodule Bob do
   def hey(input) do
     cond do
-      trim(input) == "" ->
-        "Fine. Be that way!"
-      question?(input) && shouting?(input) ->
-        "Calm down, I know what I'm doing!"
-      question?(input) ->
-        "Sure."
-      shouting?(input) ->
-        "Whoa, chill out!"
-      true ->
-        "Whatever."
+      silence?(input)           -> "Fine. Be that way!"
+      forceful_question?(input) -> "Calm down, I know what I'm doing!"
+      question?(input)          -> "Sure."
+      shouting?(input)          -> "Whoa, chill out!"
+      true                      -> "Whatever."
     end
   end
 
-  defp question?(input) do
-    ends_with?(input, "?")
-  end
+  defp silence?(input), do: trim(input) == ""
 
-  defp shouting?(input) do
-    upcase(input) == input && upcase(input) != downcase(input)
-  end
+  defp forceful_question?(input), do: question?(input) && shouting?(input)
+
+  defp question?(input), do: input |> ends_with?("?")
+
+  defp shouting?(input), do: upcase(input) == input && upcase(input) != downcase(input)
 end
