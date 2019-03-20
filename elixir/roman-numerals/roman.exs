8b3a3d1c7b83578ddfd94034eeb815c41cc0206a
eux <euxuuu@gmail.com>
2019-03-21 00:16:19 +0800

    Improve Elixir Track - roman numerals

--- elixir/roman-numerals/roman.exs
+++ elixir/roman-numerals/roman.exs
@@ -4,19 +4,19 @@ defmodule Roman do
   """
 
   @roman_base [
-    {1000, "M"},
-    {900, "CM"},
-    {500, "D"},
-    {400, "CD"},
-    {100, "C"},
-    {90, "XC"},
-    {50, "L"},
-    {40, "XL"},
-    {10, "X"},
-    {9, "IX"},
-    {5, "V"},
-    {4, "IV"},
-    {1, "I"}
+    1000, "M",
+    900, "CM",
+    500, "D",
+    400, "CD",
+    100, "C",
+    90, "XC",
+    50, "L",
+    40, "XL",
+    10, "X",
+    9, "IX",
+    5, "V",
+    4, "IV",
+    1, "I"
   ]
 
   @spec numerals(pos_integer) :: String.t()
@@ -26,9 +26,9 @@ defmodule Roman do
 
   defp to_numerals([], _, romans), do: romans
 
-  defp to_numerals([{number, letter} | remain_base], remainder, romans)
+  defp to_numerals([number, letter | remain_base], remainder, romans)
   when div(remainder, number) > 0 do
-    romans = "#{romans}#{String.duplicate(letter, div(remainder, number))}"
+    romans = romans <> String.duplicate(letter, div(remainder, number))
     remainder = rem(remainder, number)
     to_numerals(remain_base, remainder, romans)
   end
