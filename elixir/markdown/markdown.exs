2729ba68d7d9c61d1e38462ddf4658714c6c1e96
eux <euxuuu@gmail.com>
2019-04-21 10:08:39 +0800

    Elixir Track - markdown improve

--- elixir/markdown/markdown.exs
+++ elixir/markdown/markdown.exs
@@ -12,58 +12,24 @@ defmodule Markdown do
   """
   @spec parse(String.t()) :: String.t()
   def parse(markdown) do
-    String.split(markdown, "\n")
+    markdown
+    |> String.split("\n")
     |> Enum.map_join(&process/1)
+    |> parse_emphasis_tags()
     |> wrap_ul_tag()
   end
 
-  defp process("#" <> _ = header), do: parse_header(header)
-  defp process("*" <> _ = list), do: parse_list_md_level(list)
-  defp process(line), do: String.split(line) |> enclose_with_paragraph_tag()
+  defp process("#" <> text), do: parse_header(text, 1)
+  defp process("* " <> text), do: "<li>#{text}</li>"
+  defp process(line), do: "<p>#{line}</p>"
 
-  defp parse_header(header) do
-    parse_header_md_level(header) |> enclose_with_header_tag()
-  end
+  defp parse_header(" " <> text, level), do: "<h#{level}>#{text}</h#{level}>"
+  defp parse_header("#" <> text, level), do: parse_header(text, level + 1)
 
-  defp parse_header_md_level(header) do
-    [heading_signs | words] = String.split(header)
-    {String.length(heading_signs) |> to_string(), Enum.join(words, " ")}
-  end
-
-  defp parse_list_md_level(list) do
-    text =
-      String.trim_leading(list, "* ")
-      |> String.split()
-      |> join_words_with_tags()
-    "<li>#{text}</li>"
-  end
-
-  defp enclose_with_header_tag({level, text}) do
-    "<h#{level}>#{text}</h#{level}>"
-  end
-
-  defp enclose_with_paragraph_tag(words) do
-    "<p>#{join_words_with_tags(words)}</p>"
-  end
-
-  defp join_words_with_tags(words) do
-    Enum.map_join(words, " ", &replace_md_with_tag/1)
-  end
-
-  defp replace_md_with_tag(word) do
-    replace_prefix_md(word) |> replace_suffix_md()
-  end
-
-  defp replace_prefix_md(word) do
-    word
-    |> String.replace(~r/^__/, "<strong>")
-    |> String.replace(~r/^_/, "<em>")
-  end
-
-  defp replace_suffix_md(word) do
-    word
-    |> String.replace(~r/__/, "</strong>")
-    |> String.replace(~r/_/, "</em>")
+  defp parse_emphasis_tags(text) do
+    text
+    |> String.replace( ~r/__([^_]+)__/, "<strong>\\1</strong>")
+    |> String.replace( ~r/_([^_]+)_/, "<em>\\1</em>")
   end
 
   defp wrap_ul_tag(line) do
